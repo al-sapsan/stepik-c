cb114b7868707e48120db788a7c17147d3ffb341
Oleg Sokolov <al.sapsan@yahoo.com>
2025-05-06 17:59:39 +0300

    add all files

---
+++ code/src/switch_case_char.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    char item;
+ 
+    if(scanf("%c", &item) != 1) {
+        printf("Error input");
+        return 0;
+    }
+ 
+    switch(item) {
+    case 'a': // Переход к нижнему оператору, так как отсутвует выражение
+    case 'A': // Дальнейший переход к нижнему оператору (printf)
+        printf("Symbol A\n");
+        break;
+    case 'b':
+    case 'B':
+        printf("Symbol B\n");
+        break;
+    case 'c':
+    case 'C':
+        printf("Symbol C\n");
+        break;
+    }
+ 
+    return 0;
+}
