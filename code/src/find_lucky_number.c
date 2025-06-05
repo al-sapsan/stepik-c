56e5aa92d5202a5daf8bcf62e9345d72497e1e58
Oleg Sokolov <al.sapsan@yahoo.com>
2025-06-05 11:18:19 +0300

    add find*.c files

--- code/src/find_lucky_number.c
+++ code/src/find_lucky_number.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+/**
+ * @brief Проверяет, является ли шестизначное число "счастливым".
+ *
+ * Если число не шестизначное, выводится "ERROR".
+ *
+ * @return 0 при успешном завершении
+ */
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1 || n < 100000 || n > 999999) {
+        printf("ERROR\n");
+        return 0;
+    }
+    int sum1 = n/100000 + (n/10000)%10 + (n/1000)%10;
+    int sum2 = (n/100)%10 + (n/10)%10 + n%10;
+    printf("%s\n", (sum1 == sum2) ? "YES" : "NO");
+    return 0;
+}
 
+/*
 int main(void)
 {
     int n, sum1 = 0, sum2 = 0;
@@ -18,3 +37,4 @@ int main(void)
     printf("%s\n", (sum1 == sum2) ? "yes" : "no");
     return 0;
 }
+    */
